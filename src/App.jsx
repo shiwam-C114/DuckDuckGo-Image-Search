b2e529dc66b99b9d68c9b1539c8b383e8507d4c0
shiwam-C114 <zaqqst@gmail.com>
2022-08-22 08:47:01 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,69 @@
+import {
+  Center,
+  Container,
+  Flex,
+  Heading,
+  Input,
+  Stack,
+} from '@chakra-ui/react';
+import { useState } from 'react';
+import Carousel from './Carousel';
+
+function App() {
+  const [gotImg, setgotImg] = useState(false);
+  const [query, setQuery] = useState('');
+  const [provider, setProvider] = useState('google');
+  const [page, setPage] = useState('100');
+  const [data, setData] = useState([]);
+
+  async function duckduckgo(e) {
+    if (e.key === 'Enter') {
+      fetch(`https://duckduckgo.com/?q=${query}&iar=images`)
+        .then((r) => r.text())
+        .then((data) => {
+          let vqd = data.split(';').find((e) => e.includes('vqd='));
+          vqd = vqd.split("'").join('');
+          console.log(vqd);
+          fetch(
+            `https://duckduckgo.com/i.js?q=${query}&o=json&p=1&s=${page}&u=${provider}&` +
+              vqd
+          )
+            .then((r) => r.json())
+            .then((data) => {
+              console.log(data);
+              setData(data.results);
+              setgotImg((prev) => !prev);
+            });
+        });
+    }
+  }
+
+  return (
+    <>
+      {!gotImg ? (
+        <Container mt={'45vh'} w={'100vw'}>
+          <Stack padding={'0 0 40px 0'} textAlign={'center'}>
+            <Heading>
+              Search Image <br />
+            </Heading>
+          </Stack>
+          <Stack>
+            <Input
+              onChange={(e) => {
+                setQuery(e.target.value);
+              }}
+              onKeyDown={duckduckgo}
+              width={'100%'}
+            />
+          </Stack>
+        </Container>
+      ) : (
+        <>
+          <Carousel data={data} />
+        </>
+      )}
+    </>
+  );
+}
+
+export default App;
---
+++ src/Nav.jsx
@@ -0,0 +1,44 @@
+import { ReactNode } from 'react';
+import {
+  Box,
+  Flex,
+  Avatar,
+  Link,
+  Button,
+  Menu,
+  MenuButton,
+  MenuList,
+  MenuItem,
+  MenuDivider,
+  useDisclosure,
+  useColorModeValue,
+  Stack,
+  useColorMode,
+  Center,
+  Heading,
+} from '@chakra-ui/react';
+import { MoonIcon, SunIcon } from '@chakra-ui/icons';
+
+export default function Nav() {
+  const { colorMode, toggleColorMode } = useColorMode();
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  return (
+    <>
+      <Box bg={useColorModeValue('gray.100', 'gray.900')} px={4}>
+        <Flex h={16} alignItems={'center'} justifyContent={'space-between'}>
+          <Box>
+            <Heading>Search Image</Heading>
+          </Box>
+
+          <Flex alignItems={'center'}>
+            <Stack direction={'row'} spacing={7}>
+              <Button onClick={toggleColorMode}>
+                {colorMode === 'light' ? <MoonIcon /> : <SunIcon />}
+              </Button>
+            </Stack>
+          </Flex>
+        </Flex>
+      </Box>
+    </>
+  );
+}
